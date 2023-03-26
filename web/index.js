fcbc6de058b822e839c016016e7bd7df747a2218
dharmik2319 <126943942+dharmik2319@users.noreply.github.com>
2023-03-26 15:39:45 +0530

    made the embedded editor work

--- web/index.js
+++ web/index.js
@@ -2,6 +2,17 @@ var editor = ace.edit("editor");
 editor.setTheme("ace/theme/twilight");
 editor.session.setMode("ace/mode/html");
 editor.setReadOnly(true);
+editor.setAutoScrollEditorIntoView(true);
+function makeAceEditorResizable(editor){
+	// mouseup = css resize end
+	document.addEventListener("mouseup", function(e){
+		editor.resize();
+	});
+}
+
+var aceEditorInstance = ace.edit("editor");
+makeAceEditorResizable(aceEditorInstance);
+
 
 function fetchHtml() {
 	fetch(window.location.href)
@@ -9,7 +20,29 @@ function fetchHtml() {
 	return response.text();
 	})
 	.then((html) => {
-	editor.setValue(html)     
-	});
+	editor.setValue(html)
+	})
+	
 }
-window.onload = fetchHtml();
+
+function putLines(lines) {
+	editor.setOption("maxLines", lines);
+}
+/* abandon all hope if you wish to understand this
+ * dont touch it as well, if it aint broke, dont fix it*/
+function resolveAfter2Seconds() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 200);
+  });
+}
+
+async function asyncCall() {
+  fetchHtml();
+  const result = await resolveAfter2Seconds();
+  putLines(editor.getValue().match(/\n/g).length + 1);  // Expected output: "resolved"
+  document.getElementById('editor').style.fontSize='13px';
+}
+
+asyncCall();
